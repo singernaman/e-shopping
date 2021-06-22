55a5161f4558eb66a2c0df5edf8b7aa9592e8bc1
Naman Patel <nmnpatel001@gmail.com>
2021-06-22 13:22:16 +0530

    Add project files.

---
+++ e-shopping/Models/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace e_shopping.Models
+{
+    public class DatabaseContext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+
+        }
+    }
+}
---
+++ e-shopping/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e_shopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
